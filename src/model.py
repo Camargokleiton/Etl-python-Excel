1df723bb490e0d0ca62d0e8ef719123955138bd1
Kleiton camargo <kleiton_camargo@hotmail.com>
2025-08-26 15:13:20 -0300

    Commit removing src errors

--- src/model.py
+++ src/model.py
@@ -23,11 +23,3 @@ def validate_data(file):
     
     return results
 
-if __name__ == "__main__":
-    st.title("CSV Data Validator")
-    uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
-    
-    if uploaded_file is not None:
-        validation_results = validate_data(uploaded_file)
-        for result in validation_results:
-            st.write(result)
